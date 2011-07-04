a3a8705609d24f1db2ba8216c58f44ce07a3caeb
enjalot <enjalot+github@gmail.com>
2011-07-04 13:02:44 -0700

    fixed up json to match arduino

--- arduino/views.py
+++ arduino/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 import json
+import datetime
 #from django.core import serializers
 
 
@@ -8,9 +9,20 @@ from arduino.models import Report
 def index(request):
     return HttpResponse("HI")
 
+"""
+{ "report": 
+    { 
+    "initialize":{"initializing":0, "baudRate":38400}, 
+    "arduino_uptime": "0:00:51:32", 
+    "tank_level_sensor": { "full":0}, 
+    "flow_switch_sensor": { "flow":1}, 
+    "flow_rate_sensor": { "flowRate":1.62} 
+    } 
+}
+"""
 def upload(request):
     jsonreport = request.REQUEST["jsonreport"]
-    print jsonreport
+    #print jsonreport
     #example json string: 
     #http://localhost:8000/arduino/upload?jsonreport={%22garden%22:%22demo%22,%22date%22:%222011-06-18%2007:40:46%22,%22rate%22:1.6,%22water%22:1,%22tank%22:1,%22lights%22:1}
     #ss = serializers.deserialize("json", jsonreport)
@@ -19,14 +31,21 @@ def upload(request):
     #    print "obj:"
     #    print obj
     js = json.loads(jsonreport)
+    jsr = js["report"]
+    print jsr["flow_switch_sensor"]
+    #print jsr
     #print jsono
     #this should be automated by django
-    rep = Report(   garden=js['garden'], 
-                    date=js['date'],
-                    rate=js['rate'],
-                    water=js['water'],
-                    tank=js['tank'],
-                    lights=js['lights'])
+    rep = Report(   #garden=js['garden'], 
+                    garden="demo",
+                    #date=js['date'],
+                    date=datetime.datetime.now(),
+                    arduino_uptime=jsr['arduino_uptime'],
+                    flow_switch_sensor=jsr['flow_switch_sensor']['flow'],
+                    flow_rate=jsr['flow_rate_sensor']['flowRate'],
+                    tank_level_sensor=jsr['tank_level_sensor']['full'],
+                    #lights=js['lights'])
+                    )
     print rep
     rep.save()
 
@@ -35,16 +54,16 @@ def upload(request):
 
 
 def latest(request):
-    garden = 'demo'
 
     reports = """ """
     latest_reports = Report.objects.all().order_by('-date')[:5]
     for report in latest_reports:
-        reports += "%s | %d | %d | %d | %d <br>" % (report.date, report.rate, report.water, report.tank, report.lights)
+        reports += "%s | %s | %s | %f | %d | %d <br>" % (report.garden, report.date, report.arduino_uptime, report.flow_rate, report.flow_switch_sensor, report.tank_level_sensor)#, report.lights)
 
     s = """
-    <html><head><title>Latest Garden Reports from [ %(garden)s ]</title></head>
-    Reports from %(garden)s:<br>
+    <html><head><title>Latest Garden Reports</title></head>
+    Reports: <br>
+    garden | date | arduino_uptime | flow rate | flow switch sensor | tank level sensor <br>
     %(reports)s
-    """ % ({'garden':garden, 'reports':reports})
+    """ % ({'garden':"ASDF", 'reports':reports})
     return HttpResponse(s)
